122b50978451a4cbee9898ba97d6dc534703e5f8
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-06-09 15:20:38 +0300

    added styling for fonts and header

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-
-$(document).ready(function(){
-  $("form#form").submit(function(event){
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
     event.preventDefault();
-    var number=parseInt($("input#userinput").val());
-    var newArray=[];
-    var newerArray=[];
-    for (var index=1;index<=number;index+=1){
-    newArray.push(index)};
-    for(var indices=1;indices<=number;indices+=1){
-    if(newArray[indices]%15===0){newArray[indices]="pingpong"}
-    else if(newArray[indices]%5===0){newArray[indices]="pong"}
-    else if(newArray[indices]%3===0){newArray[indices]="ping"}
-    newerArray.push(newArray[indices]);
+    var number = parseInt($("input#userinput").val());
+    var newArray = [];
+    var newerArray = [];
+    for (var index = 1; index <= number; index += 1) {
+      newArray.push(index)
+    };
+    for (var indices = 1; indices <= number; indices += 1) {
+      if (newArray[indices] % 15 === 0) {
+        newArray[indices] = "pingpong"
+      } else if (newArray[indices] % 5 === 0) {
+        newArray[indices] = "pong"
+      } else if (newArray[indices] % 3 === 0) {
+        newArray[indices] = "ping"
+      }
+      newerArray.push(newArray[indices]);
     }
 
     $("#results").text(number);
